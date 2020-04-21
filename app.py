b2527be0590b8bd51b8a931f3c5519b7b8a999d1
cailean17@gmail.com <cailean17@gmail.com>
2020-04-21 09:14:03 -0700

    Edited App.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+    return "Hello World"
 
 if __name__ == '__main__':
     app.run(debug=True, ssl_context = "adhoc")
