97cf485d19c040c2881fc96d56850a4d41481ca0
[Boyuan Liu] <50649079+boyuan12@users.noreply.github.com>
2020-04-24 22:52:23 -0700

    added README.md, updated app.py all contents, updated index.html with template inheritance

---
+++ README.md
@@ -0,0 +1,51 @@
+## Carbon Footprint
+This is a small website that can unveil your unknown contribution to climate change.
+
+## Motivation
+This project was first created during 2020 Silicon Valley Hacks. Also during COVID-19 Pandemic. This website was also created on 2020 Earth Day. We want to unveil your contribution to climate change.
+
+## Build status
+[![Build status](https://ci.appveyor.com/api/projects/status/cgas7tr8q7dkcp8j?svg=true)](https://ci.appveyor.com/project/boyuan12/carbon-footprint)
+ 
+## Screenshots
+
+
+## Tech/framework used
+**Built With**
+Flask - A micro-framework build in Python for web development
+
+## Features
+This project include following features:
+1. You can see today's air quality in a map
+2. Register/login (Google)
+3. You can submit activities you did throughout the day, then it will give you how many co2 you emitted
+4. You can view your history logs.
+
+## Code Example
+TODO
+
+## Installation
+To install this website, first make sure you have git installed, then you can clone this repository first by typing: `git clone https://github.com/cailean17/Carbon-Footprint`. Then `cd Carbon-Footprint`, which change your directory to this project's directory. Then make sure you have Python and pip installed. Then run `pip3 install -r requirements.txt`. Which going to install all dependency for you. Then run this command to setup Flask environment: `export FLASK_APP=app.py` (Mac) or `set FLASK_APP=app.py` (Windows). Finally, you can run `flask run`, then you can open up your favorite browser and go to http://127.0.0.1:5000/.
+
+## API Reference
+There's no API available.
+
+## How to use?
+TODO
+
+## Contribute
+We appreciate everyone's generosity to contribute to this project, you can find all issues on the issues tab above. If you want to add an issue, make sure you describe the issue as detail as possible, add screenshots if needed. To work on an issue, make sure you reply to the issue first. Then you can create a Pull Request.
+
+## Credits
+@cailean17 @boyuan12
+
+## License
+MIT License
+
+Copyright (c) 2020 @cailean17 and @boyuan12
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import json
 import os
 import datetime
 import time
+import json
 
 from flask import Flask, request, render_template, jsonify, redirect, flash, url_for, session
 from sqlalchemy import create_engine
@@ -37,10 +37,17 @@ db = scoped_session(sessionmaker(bind=engine))
 s = db()
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def homepage():
-    """Displays the homepage."""
-    return render_template('index.html')
+    if request.method == 'POST':
+      """results = {"Homepage": "This is the <a href='/'>homepage</a> for this website with a map that have pollution level.", 
+                 "Map": "Map is showing on the <a href='/'>homepage</a>. With pollution level",
+                 "Register": "<a href='/register'>Register</a> an account is nesscary if you want to access some service.", 
+                 "Carbon Footprint": ""}"""
+      pass
+    else:
+      """Displays the homepage."""
+      return render_template('index.html')
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -57,17 +64,17 @@ def register():
     for row in result:
         row_as_dict = dict(row)
         listT.append(row_as_dict)
-
+        
     if len(listT) != 0:
       flash("Already registered")
       return redirect('/register')
-
+  
     if password != confirm:
       flash('Please double check your password', 'danger')
-
+      
     s.execute('INSERT INTO users (username, password, email) VALUES (:username, :password, :email)',  {'username': username, 'password': generate_password_hash(password), 'email': str(email)})
     s.commit()
-
+    
     return render_template('success.html', message='Registered Successfully')
 
   else:
@@ -76,20 +83,20 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-
+  
   if request.method == 'POST':
     username = request.form.get('username')
-
+    
     result = s.execute("SELECT * FROM users WHERE username = :username LIMIT 1", {'username': str(username)})
     listT = []
     for row in result:
         row_as_dict = dict(row)
         listT.append(row_as_dict)
-
+  
     try:
       if listT[0]["password"] == 'Google':
         return redirect('/google')
-
+    
       if check_password_hash(listT[0]['password'], request.form.get("password")):
           session['user_id'] = listT[0]['id']
           return render_template('activities.html', username=username)
@@ -100,7 +107,7 @@ def login():
 
     flash('Wrong password', category='danger')
     return redirect('/login')
-
+    
   else:
     return render_template('register.html')
 
@@ -113,37 +120,37 @@ def logout():
 
     # redirect to homepage
     return redirect("/")
-
+  
 
 @app.route("/google")
 def login_google():
     def get_google_provider_cfg():
         return requests.get(GOOGLE_DISCOVERY_URL).json()
-
+   
     google_provider_cfg = get_google_provider_cfg()
     authorization_endpoint = google_provider_cfg["authorization_endpoint"]
 
-
+   
     request_uri = client.prepare_request_uri(
         authorization_endpoint,
         redirect_uri=request.base_url + "/callback",
         scope=["openid", "email", "profile",],
     )
     return redirect(request_uri)
-
+  
 @app.route("/google/callback")
 def callback():
 
-
+   
     code = request.args.get("code")
 
-
+  
     def get_google_provider_cfg():
         return requests.get(GOOGLE_DISCOVERY_URL).json()
 
     google_provider_cfg = get_google_provider_cfg()
     token_endpoint = google_provider_cfg["token_endpoint"]
-
+   
     token_url, headers, body = client.prepare_token_request(
         token_endpoint,
         authorization_response=request.url,
@@ -154,14 +161,14 @@ def callback():
         headers=headers,
         data=body,
         auth=(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET), )
-
+   
     client.parse_request_body_response(json.dumps(token_response.json()))
-
-
+    
+   
     userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
     uri, headers, body = client.add_token(userinfo_endpoint)
     userinfo_response = requests.get(uri, headers=headers, data=body)
-
+   
     if userinfo_response.json().get("email_verified"):
         global unique_profile_pic_src
 
@@ -169,12 +176,11 @@ def callback():
         users_email = userinfo_response.json()["email"]
         picture = userinfo_response.json()["picture"]
         users_name = userinfo_response.json()["given_name"]
-
+        unique_profile_pic_src =  User.get_user_profilepic(users_email)
         users_last_name = userinfo_response.json()["family_name"]
-        return redirect(url_for("activities"))
         # print(unique_profile_pic_src)
         result = s.execute("SELECT * FROM users WHERE email = :email LIMIT 1", {'email': str(users_email)})
-
+        
         listT = []
         for row in result:
             row_as_dict = dict(row)
@@ -183,47 +189,47 @@ def callback():
         if len(listT) != 0:
           flash("Already registered")
           return redirect('/register')
-
+        
         s.execute("INSERT INTO users (username, password, email) VALUES (:username, :password, :email)", {"username": users_name, "password": "Google", "email": users_email})
         s.commit()
         return redirect(url_for("register"))
-
+      
     else:
         return "User email not available or not verified by Google.", 400
-
+      
 
 @app.route('/activities', methods=['GET', 'POST'])
 @login_required
-def activities():
-
+def activites():
+  
   # Featured snippet from the web In an ordinary trip with minimal traffic, you're likely to emit around 0.7 pounds of carbon dioxide per mile traveled. If your average speed drops to 15 miles per hour, that emissions amount rises to 1.2 pounds. ... Congestion was heavy, with more than half the cars traveling less than 25 miles per hour
   # The U.S. Environmental Protection Agency just released rules limiting particulate emissions of wood stoves sold after January 1, 2016 to 4.5 grams per hour, or three to ten times less than the 15 to 40 grams per hour that an older stove emits.
   # 66 grams of Co2 emitted per kilowatt hour for ... an oven and a gas stove
   # 40lbs co2 per lbs landfills
-
+  
   if request.method == 'POST':
     ts = time.time()
     car_pollution = float(request.form.get("car-hours")) * float(request.form.get("car-mile")) * 0.7 * 454 # grams
     wood_pollution = float(request.form.get('wood-hours')) * 4.5 # grams
     gas_pollution = float(request.form.get("gas-hours")) * 66 # grams
     trash_pollution = float(request.form.get("trash-pounds")) * 40 * 454 # grams
-
+    
     total = car_pollution + wood_pollution + gas_pollution + trash_pollution
     timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
     info = {'timestamp': str(timestamp), 'description': 'Car Pollution: {}g <br>Wood Pollution: {}g <br>Gas Pollution: {}g <br>Trash pollution: {}g'.format(car_pollution, wood_pollution, gas_pollution, trash_pollution), 'user_id': session.get('user_id'), 'total_carbon': total}
     s.execute('INSERT INTO logs (timestamp, description, user_id, total_carbon) VALUES (:timestamp, :description, :user_id, :total_carbon)', info)
     s.commit()
-
+    
     return render_template("activity-complete.html", description=info["description"], total=total)
-
+    
   else:
-
+    
     return render_template('activities.html')
 
 @app.route('/logs')
 @login_required
 def logs():
-
+  
   results = s.execute("SELECT * FROM logs WHERE user_id = :user_id", {"user_id": session.get('user_id')})
   return render_template('logs.html', results=results)
 
